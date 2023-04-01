7010361d12f28ffa3ad115462822b88664184510
fishyfishfishfish <114358433+fishyfishfishfish@users.noreply.github.com>
2023-03-31 20:44:05 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+noseX=0;
+noseY=0;
+leftWristX=0;
+rightWristX=0;
+difference=0;
+
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,500);
+    canvas=createCanvas(550,500);
+canvas.position(560,150);
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("model is loaded");
+}
+
+function gotPoses(results){
+    console.log(results);
+}
